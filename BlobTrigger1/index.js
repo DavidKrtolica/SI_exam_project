bdb0081cae55cd4ec74dbef4f1f55ae5791489cc
dim <dimitris.giokas1@gmail.com>
2022-11-15 17:48:43 +0100

    initial commit

---
+++ BlobTrigger1/index.js
@@ -0,0 +1,4 @@
+﻿module.exports = async function (context, myBlob) {
+    context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
+    if (context.bindings.inputBlob) context.bindings.outputSbMsg = context.bindings.inputBlob;
+};
