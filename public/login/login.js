46d78e369a7adeaeea9d9ffa6ec1331b30d0dcb0
David <david.krtolica8@gmail.com>
2022-11-19 15:34:38 +0100

    Connetion to the Azure-hosted AUTH_SERVICE, ready for deployment itself, LOGIN/REGISTER WORKING

--- public/login/login.js
+++ public/login/login.js
@@ -1,16 +1,31 @@
-const url = "https://authentication-service-si.azurewebsites.net/auth/login"
-async function submitLogin() {
-    await fetch(url, {
+//URL ENDPOINT FOR MAKING THE POST REQUEST TO THE EXPOSED AUTH_SERVICE OF THE OTHER GROUP
+//FOR NOW, WE ARE USING OUR OWN SERVICE HOSTED ON AZURE, SHOULD ALSO BE A SECRET/IN THE .ENV 
+const url = "https://authentication-service-si.azurewebsites.net/auth/login";
+
+//FETCH FUNCTION FOR EXECUTING THE POST REQUEST
+function submitLogin() {
+    const form = document.getElementById('login-form');
+    fetch(url, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            "email": "dk@email.com",
-            "password": "dk123456789"
+            "email": form.email.value,
+            "password": form.password.value
         })
     })
     .then(response => response.json())
-    .then(response => console.log(JSON.stringify(response)));
-}
+    .then(response => {
+        if(response.status == 'Unauthorized') {
+            alert("Error occured - "+response.description);    
+        } else {
+            //console.log(JSON.stringify(response));
+            //console.log(response);
+            //STATUS SUCCESS - WE GET ACCESS AND REFRESH TOKEN
+            console.log("THIS IS HOW YOU ACCESS THE ACCESS TOKEN: "+response.data.accessToken);
+            console.log("THIS IS HOW YOU ACCESS THE REFRESH TOKEN: "+response.data.refreshToken);
+        }
+    });
+};
---
+++ public/register/register.js
@@ -0,0 +1,36 @@
+//URL ENDPOINT FOR MAKING THE POST REQUEST TO THE EXPOSED AUTH_SERVICE OF THE OTHER GROUP
+//FOR NOW, WE ARE USING OUR OWN SERVICE HOSTED ON AZURE, SHOULD ALSO BE A SECRET/IN THE .ENV 
+const url = "https://authentication-service-si.azurewebsites.net/auth/register";
+
+//FETCH FUNCTION FOR EXECUTING THE POST REQUEST
+function submitRegister() {
+    const form = document.getElementById('register-form');
+    if(form.password.value == form.repeatpassword.value) {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "email": form.email.value,
+                "password": form.password.value
+                //WHAT OTHER DATA DO WE WANT TO SAVE???
+                //FOR FIREBASE, ONLY EMAIL AND PASSWORD NEEDED!
+            })
+        })
+        .then(response => response.json())
+        .then(response => {
+            if(response.status == 'Failed') {
+                alert("Error occured - "+response.description);    
+            } else {
+                //STATUS SUCCESS - WE GET USER ID AND EMAIL
+                //console.log(JSON.stringify(response));
+                window.location.assign("/login");
+            }
+            form.reset();
+        });
+    } else {
+        alert("Passwords do not match!");
+    }
+};
