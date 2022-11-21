3d94596745ba2bb13248d69776197af9343ddb00
georgeUrsachi <george.ursachi@lenus.io>
2022-11-21 21:53:22 +0100

    Added redis cache to Apollo server

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
 import express from 'express';
-import {
-   ApolloServer
-} from 'apollo-server-express';
-import {
-   typeDefs,
-   resolvers
-} from './gql/gql.js';
-import {
-   BlobServiceClient
-} from '@azure/storage-blob';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './gql/gql.js';
+import { BlobServiceClient } from '@azure/storage-blob';
+import Keyv from 'keyv';
+import { KeyvAdapter } from '@apollo/utils.keyvadapter';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 
 const apolloServer = new ApolloServer({
    typeDefs,
-   resolvers
+   resolvers,
+   cache: new KeyvAdapter(new Keyv(process.env.REDIS_URI)),
 });
 
 await apolloServer.start();
 apolloServer.applyMiddleware({
-   app
+   app,
 });
 
 app.post('/products-update-webhook', async (req, res) => {
-   const header = req.get("Aeg-Event-Type");
+   const header = req.get('Aeg-Event-Type');
    if (header && header === 'SubscriptionValidation') {
       const event = req.body[0];
-      const isValidationEvent = event?.data?.validationCode &&
-         event?.eventType == 'Microsoft.EventGrid.SubscriptionValidationEvent'
+      const isValidationEvent =
+         event?.data?.validationCode &&
+         event?.eventType == 'Microsoft.EventGrid.SubscriptionValidationEvent';
       if (isValidationEvent) {
          return res.send({
-            "validationResponse": event.data.validationCode
-         })
+            validationResponse: event.data.validationCode,
+         });
       }
    }
 
@@ -44,27 +43,31 @@ app.post('/products-update-webhook', async (req, res) => {
 
    const containerName = process.env.CONTAINER_NAME;
 
-   const containerClient = await blobServiceClient.getContainerClient(containerName);
+   const containerClient = await blobServiceClient.getContainerClient(
+      containerName
+   );
 
-   downloadBlobToFile(containerClient, 'products.db', './products.db').then((res) => {
-      console.log('File downloaded successfully ', res);
-   }).catch((e) => {
-      console.log('Error while downloading file: ', e)
-   })
+   downloadBlobToFile(containerClient, 'products.db', './products.db')
+      .then((res) => {
+         console.log('File downloaded successfully ', res);
+      })
+      .catch((e) => {
+         console.log('Error while downloading file: ', e);
+      });
 
    res.status(200).send(req.body);
-})
+});
 
 const port = process.env.PORT || 8080;
 
-app.listen({
-      port
-   }, () =>
-   console.log(`🚀 Server ready at http://localhost:${port}`)
+app.listen(
+   {
+      port,
+   },
+   () => console.log(`🚀 Server ready at http://localhost:${port}`)
 );
 
 async function downloadBlobToFile(containerClient, blobName, fileNameWithPath) {
-
    const blobClient = await containerClient.getBlobClient(blobName);
 
    await blobClient.downloadToFile(fileNameWithPath);
