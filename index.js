aff0e387f5f15e88a770a635dcd5dafd73d49fb1
dim <dimitris.giokas1@gmail.com>
2022-11-21 11:18:46 +0100

    added webhook to listen for creation of products

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { typeDefs, resolvers } from './gql/gql.js';
+import {
+   ApolloServer
+} from 'apollo-server-express';
+import {
+   typeDefs,
+   resolvers
+} from './gql/gql.js';
+import {
+   BlobServiceClient
+} from '@azure/storage-blob';
 
 const app = express();
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({
+   typeDefs,
+   resolvers
+});
 
 await apolloServer.start();
-apolloServer.applyMiddleware({ app });
+apolloServer.applyMiddleware({
+   app
+});
 
-app.listen({ port: 8080 }, () =>
+/**
+ * @param  {} '/'
+ * @param  {} (req
+ * @param  {} res
+ */
+app.post('/products-update-webhook', async (req, res) => {
+   const header = req.get("Aeg-Event-Type");
+   if (header && header === 'SubscriptionValidation') {
+      const event = req.body[0]
+      const isValidationEvent = event?.data?.validationCode &&
+         event?.eventType == 'Microsoft.EventGrid.SubscriptionValidationEvent'
+      if (isValidationEvent) {
+         return res.send({
+            "validationResponse": event.data.validationCode
+         })
+      }
+   }
+
+   const connString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+   if (!connString) throw Error('Azure Storage Connection string not found');
+
+   const blobServiceClient = BlobServiceClient.fromConnectionString(connString);
+
+   const containerName = process.env.CONTAINER_NAME;
+
+   const containerClient = await blobServiceClient.getContainerClient(containerName);
+
+   downloadBlobToFile(containerClient, 'products.db', './products.txt').then((res) => {
+      console.log('done....', res);
+   }).catch((e) => {
+      console.log(e)
+   })
+
+   res.status(200).send(req.body);
+})
+
+app.listen({
+      port: 8080
+   }, () =>
    console.log(`🚀 Server ready at http://localhost:8080`)
 );
+
+async function downloadBlobToFile(containerClient, blobName, fileNameWithPath) {
+
+   const blobClient = await containerClient.getBlobClient(blobName);
+
+   await blobClient.downloadToFile(fileNameWithPath);
+   console.log(`download of ${blobName} succeeded.`);
+}
