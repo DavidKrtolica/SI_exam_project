84faa3dfa96281a4d80198a368fc3a576bd0ddd2
georgeUrsachi <george.ursachi@lenus.io>
2022-11-22 15:20:40 +0100

    Added basic searchbar

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-    res.redirect("/login");
+app.get('/', (req, res) => {
+   res.redirect('/login');
 });
 
-app.get("/register", (req, res) => {
-    res.sendFile(__dirname+"/public/register/register.html");
+app.get('/register', (req, res) => {
+   res.sendFile(__dirname + '/public/register/register.html');
 });
 
-app.get("/login", (req, res) => {
-    res.sendFile(__dirname+"/public/login/login.html");
+app.get('/login', (req, res) => {
+   res.sendFile(__dirname + '/public/login/login.html');
 });
 
-const PORT = process.env.PORT || 8080
-const server = app.listen(PORT, error => {
-    if (error) {
-        console.log(error);
-    }
-    console.log("Server running on port", server.address().port);
+app.get('/products', (req, res) => {
+   res.sendFile(__dirname + '/public/products/products.html');
+});
+
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, (error) => {
+   if (error) {
+      console.log(error);
+   }
+   console.log('Server running on port', server.address().port);
 });
