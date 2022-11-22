31d0ab6fd5de06ce81a9c5a85e5404c282cdf5c2
dim <dimitris.giokas1@gmail.com>
2022-11-22 21:39:36 +0100

    removed html

--- index.js
+++ index.js
@@ -8,33 +8,30 @@ const app = express();
 
 const server = http.createServer(app);
 
-const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PROJECT_ID.firebaseio.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "G-MEASUREMENT_ID",
-};
+// const firebaseConfig = {
+//   apiKey: "API_KEY",
+//   authDomain: "PROJECT_ID.firebaseapp.com",
+//   databaseURL: "https://PROJECT_ID.firebaseio.com",
+//   projectId: "PROJECT_ID",
+//   storageBucket: "PROJECT_ID.appspot.com",
+//   messagingSenderId: "SENDER_ID",
+//   appId: "APP_ID",
+//   measurementId: "G-MEASUREMENT_ID",
+// };
 
 const io = new Server(server, {
     perMessageDeflate: false,
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST'],
-    },
 });
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.use(express.json());
 
-app.use(cors())
+// app.use(cors())
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    console.log('req headers = ', req.headers);
+    res.status(200).send('OK');
 })
  
 app.post('/invite', (req, res) => {
