56f052f027e98dc3de3e2ccfbb015b9a6b0840d5
Marianna <mana998@azet.sk>
2022-11-26 12:28:02 +0100

    Fixed execution of commands

--- ddl.js
+++ ddl.js
@@ -2,17 +2,18 @@ import { knexInstance as knex } from './db.js';
 
 //Use this to create a products table with one entry in case products.db is missing
 
-function createTables() {
+export function createTables() {
+    console.log("here")
     knex.schema.createTable('categories', (table) => {
         table.increments('id').primary();
-        table.string('categorytName');
-    })
+        table.string('categoryName').unique();
+    }).then()
     knex.schema.createTable('subcategories', (table) => {
         table.increments('id').primary();
-        table.string('subcategorytName');
+        table.string('subcategoryName').unique();
         table.integer('categoryId');
         table.foreign('categoryId').references('id').inTable('categories')
-    })
+    }).then()
     knex.schema.createTable('products', (table) => {
         table.increments('id').primary();
         table.string('productName');
@@ -23,23 +24,23 @@ function createTables() {
         table.string('image');
         table.string('alt');
         table.integer('categoryId')
-        table.foreign('categoryOd').references('id').inTable('subcategories')
-    })
+        table.foreign('categoryId').references('id').inTable('subcategories')
+    }).then()
     knex.schema.createTable('propertyTypes', (table) => {
         table.increments('id').primary();
-        table.string('propertyName');
-    })
+        table.string('propertyName').unique();
+    }).then()
     knex.schema.createTable('propertyValues', (table) => {
         table.increments('id').primary();
-        table.string('value');
+        table.string('value').unique();
         table.integer('propertyId');
         table.foreign('propertyId').references('id').inTable('propertyTypes')
-    })
+    }).then()
     knex.schema.createTable('productHasProperties', (table) => {
         table.increments('id').primary();
         table.integer('productId');
         table.integer('propertyId');
         table.foreign('propertyId').references('id').inTable('propertyValues')
         table.foreign('productId').references('id').inTable('products')
-    })
+    }).then()
 }
