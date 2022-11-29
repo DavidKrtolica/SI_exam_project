ffa3005cafc57eb984e14052d22d0127a75c4ca3
David Krtolica <david.krtolica8@gmail.com>
2022-11-29 15:38:21 +0100

    Code search param added when client sends a POST request to the server

--- public/register/register.js
+++ public/register/register.js
@@ -1,6 +1,12 @@
 //URL ENDPOINT FOR MAKING THE POST REQUEST TO THE EXPOSED AUTH_SERVICE OF THE OTHER GROUP
 //FOR NOW, WE ARE USING OUR OWN SERVICE HOSTED ON AZURE, SHOULD ALSO BE A SECRET/IN THE .ENV 
-const url = "https://authentication-service-si.azurewebsites.net/auth/register";
+const url = new URL("https://authentication-service-si.azurewebsites.net/auth/register");
+
+const params = new URLSearchParams(window.location.search);
+const code = params.get('code');
+if(code != undefined) {
+    url.searchParams.set('code', code);
+}
 
 //FETCH FUNCTION FOR EXECUTING THE POST REQUEST
 function submitRegister() {
