a440de9c0a7939a81fc3565e43fb9a3c664d0775
dim <dimitris.giokas1@gmail.com>
2022-12-23 16:01:04 +0100

    refactored code

--- public/wishlist/createWishlist.js
+++ public/wishlist/createWishlist.js
@@ -1,54 +1,17 @@
-// console.log("ON WISHLIST PAGE, WITH TOKENS FROM LOCAL STORAGE: \n{\n  accessToken: "+window.localStorage.getItem("accessToken")+",\n  refreshToken: "+window.localStorage.getItem("refreshToken")+"\n}");
-
-const accessToken = +window.localStorage.getItem("accessToken");
-
-const connectSocketServer = () => {
-  const socketUrl = 'wss://friends-si-exam.azurewebsites.net';
-  return io(socketUrl, {
-    extraHeaders: {
-      Authorization: accessToken ? `Bearer ${accessToken}` : `Bearer accessToken`
-    }
-  });
-}
-
-const submitCreateWishlist = async (wishlistName) => {
+const submitCreateWishlist = async () => {
+  const accessToken = +window.localStorage.getItem("accessToken");
   const input = document.getElementById('form3Example3c').value;
-  const socket = await connectSocketServer();
-  socket.emit('createWishlist', ({ wishlistName: input }))
-  socket.on('createWishlistSucceeded', () => {
-    document.getElementById('message').innerHTML="Wishlist created!";
-  })
-  socket.on('createWishlistFailed', () => {
+  fetch('https://friends-si-exam.azurewebsites.net/create-wishlist', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ accessToken: accessToken || "eyJhbGciOiJSUzI1NiIsImtpZCI6ImE5NmFkY2U5OTk5YmJmNWNkMzBmMjlmNDljZDM3ZjRjNWU2NDI3NDAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYXV0aC1zZXJ2aWNlLXNpIiwiYXVkIjoiYXV0aC1zZXJ2aWNlLXNpIiwiYXV0aF90aW1lIjoxNjY5NzQ2NTQ0LCJ1c2VyX2lkIjoiN1VsZzhrVnB5a1Z4R3lUZ09iVnNIV3BMdEpXMiIsInN1YiI6IjdVbGc4a1ZweWtWeEd5VGdPYlZzSFdwTHRKVzIiLCJpYXQiOjE2Njk3NDY1NDQsImV4cCI6MTY2OTc1MDE0NCwiZW1haWwiOiJ0ZXN0MUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdDFAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.ZRGvAccCadkznCCXp514w86ZIcd7mjmU_jAM7oB_I5QMZc0P88CfIWRV6WlZOJPQLqfb0II4wVutQyKWbDQG36Z7nI0ENdHtNWJ8N2CyQtiWKieaiXn1EwEfP8GJybQPp2iEtArp7JaPcdmrCSOqXSBLFe8qd8cQ0TXAZmsT27yAs0voLN-xYXha0Dig-9s2rWB8y2OIIEUMu9SXrgLEF7x3mNPGTPRALTFNO0fKoFwrQsf66Jc_-66q_ymN4GRqIeeDe4zvzuDxAzqRG_Vn38skWl3hdL-LQz3kswfj_gSAeujDOK1adPA4_DawELKjO4XI3tcQGNsjz1HdpACFjw", 
+    wishlistName: input }),
+  }).then(async (response) => {
+    const result = await response.json();
+    document.getElementById('message').innerHTML=`Wishlist with id ${result} created`;
+  }).catch(() => {
     document.getElementById('message').innerHTML="An error occurred.";
-  })
-}
-
-function getWishlistsByUserId() {
-  //FUNCTION WHICH FETCHES THE NECESSARY LIST OF WISHLISTS BY USER ID
-  //AND POPULATES THE <ol> IN THE HTML "myWishlists"
-}
-
-function submitInvite() {
-  const form = document.getElementById("invite-friend-form");
-  //form.inviteEmail.value
-  //FUNCTION EXECUTED WHEN A FRIEND IS INVITED BY EMAIL
-  //FROM THE "wishlist" HTML PAGE FORM
-}
-
-function getWishlistsInvitees() {
-  //SHOULD RETRIEVE ALL USERS ADDED TO A WISHLIST
-  //INLCUDING THEIR STATUS (online, offline, not registered, invite sent)
-  //AND POPULATES THE <ol> IN THE HTML "wishlist"
-}
-
-function getProductsByWishlistId() {
-  //RETIRIEVEING ALL PRODUCTS CONNECTED TO A CERTAIN WISHLIST BY ID
-  //AND POPULATES THE <ol> IN THE HTML "wishlist"
-}
-
-function submitInvite() {
-  const form = document.getElementById("invite-friend-form");
-  //form.inviteEmail.value
-  //FUNCTION EXECUTED WHEN A FRIEND IS INVITED BY EMAIL
-  //FROM THE "wishlist" HTML PAGE FORM
+  });
 }
--- public/wishlist/myWishlists.js
+++ public/wishlist/myWishlists.js
@@ -1,58 +1,57 @@
 const accessToken = +window.localStorage.getItem("accessToken");
 
-const connectSocketServer = () => {
-    const socketUrl = 'wss://friends-si-exam.azurewebsites.net';
-    return io(socketUrl, {
-        extraHeaders: {
-            Authorization: accessToken ? `Bearer ${accessToken}` : "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImE5NmFkY2U5OTk5YmJmNWNkMzBmMjlmNDljZDM3ZjRjNWU2NDI3NDAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYXV0aC1zZXJ2aWNlLXNpIiwiYXVkIjoiYXV0aC1zZXJ2aWNlLXNpIiwiYXV0aF90aW1lIjoxNjY5NzQ2NTQ0LCJ1c2VyX2lkIjoiN1VsZzhrVnB5a1Z4R3lUZ09iVnNIV3BMdEpXMiIsInN1YiI6IjdVbGc4a1ZweWtWeEd5VGdPYlZzSFdwTHRKVzIiLCJpYXQiOjE2Njk3NDY1NDQsImV4cCI6MTY2OTc1MDE0NCwiZW1haWwiOiJ0ZXN0MUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdDFAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.ZRGvAccCadkznCCXp514w86ZIcd7mjmU_jAM7oB_I5QMZc0P88CfIWRV6WlZOJPQLqfb0II4wVutQyKWbDQG36Z7nI0ENdHtNWJ8N2CyQtiWKieaiXn1EwEfP8GJybQPp2iEtArp7JaPcdmrCSOqXSBLFe8qd8cQ0TXAZmsT27yAs0voLN-xYXha0Dig-9s2rWB8y2OIIEUMu9SXrgLEF7x3mNPGTPRALTFNO0fKoFwrQsf66Jc_-66q_ymN4GRqIeeDe4zvzuDxAzqRG_Vn38skWl3hdL-LQz3kswfj_gSAeujDOK1adPA4_DawELKjO4XI3tcQGNsjz1HdpACFjw"
-        }
+const getWishlists = async () => {
+    const accessToken = +window.localStorage.getItem("accessToken");
+    fetch('https://friends-si-exam.azurewebsites.net/wishlists', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            accessToken: accessToken || "eyJhbGciOiJSUzI1NiIsImtpZCI6ImE5NmFkY2U5OTk5YmJmNWNkMzBmMjlmNDljZDM3ZjRjNWU2NDI3NDAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYXV0aC1zZXJ2aWNlLXNpIiwiYXVkIjoiYXV0aC1zZXJ2aWNlLXNpIiwiYXV0aF90aW1lIjoxNjY5NzQ2NTQ0LCJ1c2VyX2lkIjoiN1VsZzhrVnB5a1Z4R3lUZ09iVnNIV3BMdEpXMiIsInN1YiI6IjdVbGc4a1ZweWtWeEd5VGdPYlZzSFdwTHRKVzIiLCJpYXQiOjE2Njk3NDY1NDQsImV4cCI6MTY2OTc1MDE0NCwiZW1haWwiOiJ0ZXN0MUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdDFAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.ZRGvAccCadkznCCXp514w86ZIcd7mjmU_jAM7oB_I5QMZc0P88CfIWRV6WlZOJPQLqfb0II4wVutQyKWbDQG36Z7nI0ENdHtNWJ8N2CyQtiWKieaiXn1EwEfP8GJybQPp2iEtArp7JaPcdmrCSOqXSBLFe8qd8cQ0TXAZmsT27yAs0voLN-xYXha0Dig-9s2rWB8y2OIIEUMu9SXrgLEF7x3mNPGTPRALTFNO0fKoFwrQsf66Jc_-66q_ymN4GRqIeeDe4zvzuDxAzqRG_Vn38skWl3hdL-LQz3kswfj_gSAeujDOK1adPA4_DawELKjO4XI3tcQGNsjz1HdpACFjw",
+        }),
+    }).then(async (response) => {
+        const result = await response.json();
+        console.log('result =', result);
+        processResult(result);
+    }).catch((e) => {
+        console.log(e)
     });
 }
 
-const getWishlists = async () => {
-    const socket = await connectSocketServer();
-    socket.emit('getWishlists');
-
-    socket.on('getWishlistsSucceeded', ({ result }) => {
-        console.log('getWishlistsSucceeded', result);
-        const tbodyRef = document.getElementById('wishlist-table').getElementsByTagName('tbody')[0];
-        console.log('tbody ref = ', tbodyRef);
-        result.forEach(element => {
-            // Insert a row at the end of table
-            const newRow = tbodyRef.insertRow();
-            for (let i = 0; i < 4; i++) {
-                // Insert a cell at the end of the row
-                const newCell = newRow.insertCell();
-                let node;
-                switch (i) {
-                    case 0:
-                        // Append a text node to the cell - name
-                        node = document.createTextNode(element.wishlist_name);
-                        break;
-                    case 1:
-                        // Append a text node to the cell - created by
-                        node = document.createTextNode(element.created_by);
-                        break;
-                    case 2:
-                        // Append a text node to the cell - date
-                        node = document.createTextNode(new Date(element.created_at).toLocaleDateString());
-                        break;
-                    case 3:
-                        // Append a text node to the cell - action button
-                        node = document.createElement('button');
-                        node.textContent = 'View';
-                        node.id = element.wishlist_id
-                        node.onclick = navigateToWishlist;
-                        break;
-                }
-                newCell.appendChild(node);
-            }
-        });
-    })
-
-    socket.on('getWishlistsFailed', () => {
-        console.log('getWishlistsFailed');
-    })
-}
-
 getWishlists();
+
+const processResult = (result) => {
+    const tbodyRef = document.getElementById('wishlist-table').getElementsByTagName('tbody')[0];
+    result.forEach(element => {
+        // Insert a row at the end of table
+        const newRow = tbodyRef.insertRow();
+        for (let i = 0; i < 4; i++) {
+            // Insert a cell at the end of the row
+            const newCell = newRow.insertCell();
+            let node;
+            switch (i) {
+                case 0:
+                    // Append a text node to the cell - name
+                    node = document.createTextNode(element.wishlist_name);
+                    break;
+                case 1:
+                    // Append a text node to the cell - created by
+                    node = document.createTextNode(element.created_by);
+                    break;
+                case 2:
+                    // Append a text node to the cell - date
+                    node = document.createTextNode(new Date(element.created_at).toLocaleDateString());
+                    break;
+                case 3:
+                    // Append a text node to the cell - action button
+                    node = document.createElement('button');
+                    node.textContent = 'View';
+                    node.id = element.wishlist_id
+                    node.onclick = navigateToWishlist;
+                    break;
+            }
+            newCell.appendChild(node);
+        }
+    });
+}
